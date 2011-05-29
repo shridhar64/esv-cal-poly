935532a1ec406800e947939e4ea61ced76179d83
bujiie <bujiie@264903c5-de38-0cd8-8ec2-4d6bf1ea819e>
2011-05-29 01:16:00 +0000

    Motor and steering controls from matlab working

--- trunk/esv-cal-poly/Ujiie/picware/servo.c
+++ trunk/esv-cal-poly/Ujiie/picware/servo.c
@@ -105,6 +105,20 @@ void floater( int angle ) {
 }
 
 void setServoAngleInt( unsigned int angle ) {
+	float fAngle = (float)angle;
+	float setAngle = 0.0;
+	float fPercent = 0.0;
+	unsigned int duty;
+
+	fPercent = fAngle / 240.0;
+	setAngle = fPercent*( 2150.0 - 1400.0 ) + 1400.0;
+
+	duty = (unsigned int)setAngle;
+
+	setServoPWM( duty );
+}
+
+void setServoAngleInt3( unsigned int angle ) {
 	float percent;
 	float fangle;
 	float temp;
